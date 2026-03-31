6945ee4eb774e87ef493f57e5a509c1a1ead6420
vortexshopbd <raihanjoy187@gmail.com>
2026-04-01 11:03:30 +1300

    Lab Class tasks

---
+++ t1.py
@@ -0,0 +1,4 @@
+#importing function
+from t1_function import marks
+#passing value
+mk= marks(70,80,40,10,80,30)
---
+++ t1_function.py
@@ -0,0 +1,17 @@
+#defining function
+def marks(*args):
+    #initializing Sum
+    sum = 0
+    #calcualtion using for loop for all numbers in args
+    for i in args:
+        #get total
+        sum += i
+        #calculating average
+    avg = sum / len(args)
+    #printing average
+    print('Average Mark=',avg)
+    #checking pass or fail
+    if(avg>=50):
+        print('Pass')
+    else:
+        print('Fail')
